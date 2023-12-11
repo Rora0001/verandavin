8c5a4c9b582d9a6adaf5c788ac60d213c9aa0b1a
Ronja <roba0001@stud.kea.dk>
2023-12-11 22:14:19 +0100

    ronja

--- prettier.config.js
+++ prettier.config.js
@@ -3,5 +3,9 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   printWidth: 80,
+  'bracket-pair-colorizer-2.colors': ['#ff0000', '#00ff00', '#0000ff'],
+  'bracket-pair-colorizer-2.showBracketsInGutter': true,
+  'bracket-pair-colorizer-2.showBracketsInRuler': true,
+
   // Add other Prettier configurations as needed
 }
