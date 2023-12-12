dec9d96736c779af70f211f9215512ff11a2817b
Ronja <roba0001@stud.kea.dk>
2023-12-12 17:35:26 +0100

    helt done press ronja

--- .vscode/prettier.config.js
+++ .vscode/prettier.config.js
@@ -3,7 +3,7 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   printWidth: 80,
-  'bracket-pair-colorizer-2.colors': ['#ff0000', '#00ff00', '#0000ff'],
-  'bracket-pair-colorizer-2.showBracketsInGutter': true,
-  'bracket-pair-colorizer-2.showBracketsInRuler': true,
-}
+  'bracket-pair-colorizer-2.colors': ['#ff0000', '#00ff00', '#0000ff',
+  'bracket-pair-colorizer-2.showBracketsInGutter', true,
+  'bracket-pair-colorizer-2.showBracketsInRuler', true,
+},
