c3494d7a2985fc03dcaf44fb5f3e18391f7878bf
Rose-Marie <rora0001@stud.kea.dk>
2023-12-12 16:52:08 +0100

    rose

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const animatedImages = document.querySelectorAll('.animated-image')
+const popImage = document.getElementById('popImage')
 
-window.addEventListener('scroll', () => {
-  animatedImages.forEach((image) => {
-    const imageTop = image.getBoundingClientRect().top
-    const windowHeight = window.innerHeight
+function handleScroll() {
+  const scrollPosition = window.scrollY
+  const imagePosition = popImage.offsetTop
 
-    if (imageTop < windowHeight * 0.75) {
-      image.style.opacity = 1
-      image.style.transform = 'translateY(0)'
-    } else {
-      image.style.opacity = 0
-      image.style.transform = 'translateY(50px)' // Just an example, adjust as needed
-    }
-  })
-})
+  if (scrollPosition > imagePosition - window.innerHeight / 2) {
+    popImage.style.opacity = 1
+    popImage.classList.add('pop-animation')
+  }
+}
+
+window.addEventListener('scroll', handleScroll)
+
+const popImage2 = document.getElementById('popImage2')
+
+function handleScroll2() {
+  const scrollPosition = window.scrollY
+  const imagePosition = popImage2.offsetTop
+
+  // Tidsforsinkelse på 500 ms (0.5 sekunder)
+  const delay = 1000
+
+  if (scrollPosition > imagePosition - window.innerHeight / 2) {
+    // Brug setTimeout til at forsinke visningen af det andet billede
+    setTimeout(() => {
+      popImage2.style.opacity = 1
+      popImage2.classList.add('pop-animation')
+    }, delay)
+  }
+}
+
+window.addEventListener('scroll', handleScroll2)
