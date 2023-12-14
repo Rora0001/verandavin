b2fd2f4f0dc22e9d74e6c9f5fadc0145d8a2328a
Ronja <roba0001@stud.kea.dk>
2023-12-14 19:16:08 +0100

    fjernet kommentarer fra css og js ronja

--- pressimg.js
+++ pressimg.js
@@ -1,5 +1,5 @@
 
-// PRESS
+
 const popImage3 = document.getElementById('popImage3')
 
 function handleScroll() {
@@ -20,11 +20,11 @@ function handleScroll2() {
   const scrollPosition = window.scrollY
   const imagePosition = popImage4.offsetTop
 
-  // Tidsforsinkelse på 500 ms (0.5 sekunder)
+  
   const delay = 10
 
   if (scrollPosition > imagePosition - window.innerHeight / 2) {
-    // Brug setTimeout til at forsinke visningen af det andet billede
+   
     setTimeout(() => {
       popImage4.style.opacity = 1
       popImage4.classList.add('pop-animation')
