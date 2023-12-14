c370abc58902d0e7bd4cf4915c86f358ba0505fc
Ronja <roba0001@stud.kea.dk>
2023-12-14 11:39:15 +0100

    hej

--- maane.js
+++ maane.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Hent billedet med ID 'maaneImg'
+ 
   var maaneImg = document.getElementById("maaneImg");
 
-  // Tilføj CSS-klasse for rotationseffekt
+  
   maaneImg.classList.add("rotate-90-br-ccw");
 });
